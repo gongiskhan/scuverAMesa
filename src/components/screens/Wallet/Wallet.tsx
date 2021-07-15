d3b1e4a8f564ff455f096ce766a032a71cd86c62
gongiskhan <goncalo.p.gomes@googlemail.com>
2021-07-15 18:52:12 +0100

    carteira done

--- src/components/screens/Wallet/Wallet.tsx
+++ src/components/screens/Wallet/Wallet.tsx
@@ -17,26 +17,32 @@ const Wallet: React.FC = () => {
   const [user, setUser] = useState<User | null>(null);
   const [amount, setAmount] = useState<string>('');
   const [mbDetails, setMBDetails] = useState<any>(null);
+  const [loading, setLoading] = useState<boolean>(false);
+
   useEffect(() => {
     UserService.observeCurrentUser().subscribe(u => {
       setUser(u);
-      EasypayService.getEasypayPaymentMbDetailsForUser(u.uid).then(d => setMBDetails(d.method));
+      // EasypayService.getEasypayPaymentMbDetailsForUser(u.uid).then(d => setMBDetails(d.method));
     });
   }, []);
 
   const chargeMBWAY = () => {
+    setLoading(true);
     EasypayService.createEasypayPayment(user, UIDGenerator.generate(), Number(amount), 'mbw').then(r => {
       if (r.ok) {
         Alert.alert('Informação', `Foi enviado um pedido mbway para ${user?.phoneNumber} no valor de ${amount} EUR.`);
       } else {
         Alert.alert('Erro', r.statusText);
       }
+      setLoading(false);
     }).catch(err => {
       Alert.alert('Erro', err);
+      setLoading(false);
     });
   }
 
   const chargeMBREF = () => {
+    setLoading(true);
     EasypayService.createEasypayPayment(user, UIDGenerator.generate(), 0, 'mb', 'CreateFrequentPayment').then(r => {
       r.json().then(res => {
         console.log('RRR', res);
@@ -47,8 +53,10 @@ const Wallet: React.FC = () => {
       } else {
         Alert.alert('Erro', r.statusText);
       }
+      setLoading(false);
     }).catch(err => {
       Alert.alert('Erro', err);
+      setLoading(false);
     });
   }
 
@@ -69,12 +77,12 @@ const Wallet: React.FC = () => {
           marginTop: 50
         }}>Carteira: {formatCurrency(user?.wallet || 0)}</Text>
       </Container>
-      <Card title="Carregar a Carteira">
+      <Card>
         <Text style={{textAlign: 'justify', margin: 10, marginTop: 20}}>
           Carregue a sua carteira para efetuar pedidos Scuver, seja no estabelecimento ou na aplicação de entregas.
         </Text>
       </Card>
-      <Card style={{padding: 20, paddingVertical: 30}}>
+      <Card style={{height: 300}}>
         <Container style={{
           height: '100%',
           flexWrap: 'wrap',
@@ -82,26 +90,31 @@ const Wallet: React.FC = () => {
           flexDirection: 'column',
           justifyContent: 'space-between'
         }}>
-          <View style={{flex: 5, backgroundColor: '#999', flexDirection: 'row', flexShrink: 1, justifyContent: 'space-around'}}>
+          <View style={{flex: 5, flexDirection: 'row', flexShrink: 1, justifyContent: 'space-around'}}>
             <Image source={require('../../../assets/app/mbway.png')}
                    style={{flex: 1, resizeMode: 'contain', flexShrink: 1, width: 100, height: 100}}/>
           </View>
-          <Text style={{fontSize: 18, textAlignVertical: "bottom"}}>Valor a carregar:</Text>
-          <TextField
-            autoFocus
-            style={{fontSize: 18, marginLeft: 130, position: 'relative', bottom: 10}}
-            value={amount}
-            onChangeText={a => setAmount(a)}
-            placeholder="Inserir aqui"
-            placeholderTextColor={'#ccc'}
-            keyboardType="numeric"
-          />
-          <Button
-            onPress={chargeMBWAY}
-          >
-            <Text style={{color: '#fff', fontSize: 18}}>Carregar com MBWAY</Text>
-          </Button>
-          <View style={{width: '100%', marginTop: 40, flex: 2, flexDirection: 'row', alignItems: 'flex-end'}}>
+          <View style={{flex: 3, flexDirection: 'row', flexShrink: 1, justifyContent: 'space-around'}}>
+            <TextField
+              autoFocus
+              style={{fontSize: 25, flexDirection: 'row'}}
+              value={amount}
+              onChangeText={a => setAmount(a)}
+              placeholder="Valor a Carregar"
+              placeholderTextColor={'#ccc'}
+              keyboardType="numeric"
+            />
+          </View>
+          <View style={{flex: 2, flexDirection: 'row', flexShrink: 1, justifyContent: 'space-around'}}>
+            <Button
+              isLoading={loading}
+              style={{flex: 2, flexDirection: 'row'}}
+              onPress={chargeMBWAY}
+            >
+              <Text style={{color: '#fff', fontSize: 18}}>Carregar com MBWAY</Text>
+            </Button>
+          </View>
+          <View style={{width: '100%', marginTop: 10, flex: 2, flexDirection: 'row', alignItems: 'flex-end'}}>
             <Text style={{alignSelf: 'flex-end'}}>
               Sabe que pode utilizar o seu cartão de refeição com o MBWAY?
               <Text onPress={() => {
@@ -113,7 +126,7 @@ const Wallet: React.FC = () => {
           </View>
         </Container>
       </Card>
-      <Card style={{height: 250}}>
+      <Card style={{height: 230}}>
         <Container style={{
           height: '100%',
           flexWrap: 'wrap',
@@ -135,15 +148,26 @@ const Wallet: React.FC = () => {
           }}>
             {mbDetails ? (
               <View>
-                <Text style={{color: '#555', fontSize: 16}}>Ent.: {mbDetails.entity}</Text>
-                <Text style={{color: '#555', fontSize: 16}}>Ref.: {mbDetails.reference}</Text>
-                <Text style={{color: '#555', fontSize: 16}}>€: LIVRE</Text>
+                <Text style={{color: '#555', fontSize: 20}}>Ent.: {mbDetails.entity}</Text>
+                <Text style={{color: '#555', fontSize: 20}}>Ref.: {mbDetails.reference}</Text>
+                <Text style={{color: '#555', fontSize: 20}}>€: LIVRE</Text>
               </View>
-            ) : (<Button
-              onPress={chargeMBREF}
-            >
-              <Text style={{color: '#fff', fontSize: 18}}>Gerar Referência Multibanco</Text>
-            </Button>)}
+            ) : (
+              <View style={{flex: 1, flexDirection: 'row', flexShrink: 1, justifyContent: 'space-around'}}>
+                <Button
+                  isLoading={loading}
+                  style={{flex: 1, flexDirection: 'row'}}
+                  onPress={chargeMBREF}
+                >
+                  <Text style={{color: '#fff', fontSize: 18}}>Gerar Referência Multibanco</Text>
+                </Button>
+              </View>
+            )}
+          </View>
+          <View style={{width: '100%', marginTop: 10, flex: 2, flexDirection: 'row', alignItems: 'flex-end'}}>
+            <Text style={{alignSelf: 'flex-end'}}>
+              Pode demorar até 10 minutos.
+            </Text>
           </View>
         </Container>
       </Card>
